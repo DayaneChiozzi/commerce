5fc236b6e2eeb3753485df3cfbe9306445ff8dad
Dayane Chiozzi <daychiozzi@gmail.com>
2022-11-14 20:11:24 -0300

    :sparkles: feat: add campo com dateCurrent no Big Mac

---
+++ challenges/desafio16.js
@@ -0,0 +1,12 @@
+db.produtos.updateMany({
+   nome: 
+{ $eq: "Big Mac" } },
+{
+   $currentDate: { 
+  ultimaModificacao: true },
+ });
+
+db.produtos.find(
+  { ultimaModificacao: { $exists: true } },
+   { _id: 0, nome: 1 },
+);
